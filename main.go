2ddf0619af8d3abc8768a2bfc9dc801ccaa7167e
Luca Camilletti <lucamilletti@gmail.com>
2025-10-15 23:35:53 +0200

    add logger

--- main.go
+++ main.go
@@ -3,15 +3,29 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"goandjs.com/noframework/logger"
 )
+
+func initializeLogger() *logger.Logger {
+	logInstance, err := logger.NewLogger("movie.log")
+	if err != nil {
+		log.Fatalf("Failed to instance logger %v", err)
+	}
+
+	return logInstance
+}
 
 func main() {
 
+	logInstance := initializeLogger()
+	defer logInstance.Close()
 	http.Handle("/", http.FileServer(http.Dir("public")))
 
 	const addr = "localhost:8080"
 	err := http.ListenAndServe(addr, nil)
 	if err != nil {
-		log.Fatalf("Server failed: %w", err)
+		logInstance.Error("Server failed", err)
+		log.Fatalf("Server failed: %v", err)
 	}
 }
