1cb65ea92e39d573e0a6a4010914be1fa9fd2c39
Luca Camilletti <lucamilletti@gmail.com>
2025-11-08 17:33:13 +0100

    add login and register function for the form

--- public/app.js
+++ public/app.js
@@ -3,6 +3,7 @@ import "./components/AnimatedLoading.js";
 import "./components/MovieDetailsPage.js";
 import "./components/YoutubeEmbed.js";
 import { Router } from "./services/Router.js";
+import { API } from "./services/API.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   Router.init();
@@ -38,4 +39,52 @@ window.app = {
     const order = urlParams.get("order") ?? "";
     app.Router.go(`/movies?q=${q}&order=${order}&genre=${genre}`);
   },
+  register : async (event) => {
+    event.preventDefault();
+    const name = document.getElementById("register-name").value;
+    const email = document.getElementById("register-email").value;
+    const password = document.getElementById("register-password").value;
+    const confirmPassword = document.getElementById("register-password-confirmation").value;
+    const errors = [];
+    if (name.length < 2) errors.push("Name must be at least 2 characters long.");
+    if (!email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) errors.push("Email is invalid.");
+    if (password.length < 4) errors.push("Password must be at least 4 characters long.");
+    if (password !== confirmPassword) errors.push("Passwords do not match.");
+    if (errors.length > 0) {
+      app.showError(errors.join(" "), false);
+    }
+    else {
+      const response = await API.register(name, email, password);
+      if (response.success) {
+        app.Router.go("/account/");
+      }
+      else {
+        app.showError(response.message, false);
+      }
+    }
+
+  },
+    login : async (event) => {
+    event.preventDefault();
+    const email = document.getElementById("login-email").value;
+    const password = document.getElementById("login-password").value;
+    const errors = [];
+    if (!email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) errors.push("Email is invalid.");
+    if (password.length < 4) errors.push("Password must be at least 4 characters long.");
+
+    if (errors.length > 0) {
+      app.showError(errors.join(" "), false);
+    }
+    else {
+      const response = await API.login(email, password);
+      if (response.success) {
+        app.Router.go("/account/");
+      }
+      else {
+        app.showError(response.message, false);
+      }
+    }
+
+  },
+
 };
